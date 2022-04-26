6323e4d97c23ea9513e4c488a5270361c7727325
Karan Midha <karan.midha@adidas.com>
2022-04-26 14:27:16 +0530

    Added dist files for helpers

---
+++ dist/helpers/git-status/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const cp = require("child_process");
+
+module.exports.gitStatus = gitStatus;
+
+function gitStatus(cwd) {
+  return cp.spawnSync("git", ["status", "--porcelain"], {
+    cwd,
+    encoding: "utf8"
+  });
+}
